222c057600d6c3873b49f6836304a7bb26f28341
Craig Edwards <craigedwards@brainbox.cc>
2026-07-10 16:08:07 +0000

    add missing include

--- include/utf8.h
+++ include/utf8.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <cstdint>
 
 std::size_t utf8_length(unsigned char c);
 uint32_t decode_utf8(const std::string& text, std::size_t offset, std::size_t length);
